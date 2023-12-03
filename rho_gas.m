a4abe2299151e709afc3ab16a0ce7911fbef023d
JonasIIII <149978085+JonasIIII@users.noreply.github.com>
2023-12-03 20:30:24 -0300

    Update rho_gas.m

--- rho_gas.m
+++ rho_gas.m
@@ -1,7 +1,7 @@
-function rho = rho_gas(T, P)
+function rho = rho_gas(Tgas, P)
   % Constantes
   R = 287.05; % Constante específica do ar em J/(kg·K)
-  
+  T = Tgas + 273.15;
   % Calcular densidade do ar
   rho = P / (R * T);
 end
