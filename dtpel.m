da6a0a70efa0da27351c8da4476c2abfe586620b
JonasIIII <149978085+JonasIIII@users.noreply.github.com>
2023-12-03 21:12:39 -0300

    Update dtpel.m

--- dtpel.m
+++ dtpel.m
@@ -1,6 +1,6 @@
-function dtpelota = dtpel(Tpel,Tgas)
-  A=20;
-  U=0.028;
+function dtpelota = dtpel(Tpel,Tgas,Fgas)
+  A=420;
+  U=0.1*sqrt(Fgas);
   mi=172;
   dtpel=(U*A*(Tgas-Tpel))/mi;
 dtpelota=dtpel
