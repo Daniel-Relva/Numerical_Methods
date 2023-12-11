4a7ec387698b1d2d5f6cc4585385c0bc7a066ee9
JonasIIII <149978085+JonasIIII@users.noreply.github.com>
2023-12-11 10:24:12 -0300

    Update dtpel.m

--- dtpel.m
+++ dtpel.m
@@ -1,6 +1,6 @@
 function dtpelota = dtpel(Tpel,Tgas)
-  A=320;
-  U=0.028;
+  A=320; % area de transferencia de calor
+  U=0.028; % coeficiente de transferencia de calor
   mi=172; %capacidade calorifica do volume de pelotas
   dtpel=(U*A*(Tgas-Tpel))/mi;
 dtpelota=dtpel;
