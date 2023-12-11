0d5d1b574da19dd1195590519ff789f48aad8c60
EduardoF141 <149977972+EduardoF141@users.noreply.github.com>
2023-12-11 10:18:05 -0300

    Update dtgas.m

--- dtgas.m
+++ dtgas.m
@@ -3,6 +3,6 @@ U=0.028;
 A=320;
 Fgas=1.11;
 Yg=0.26;
-dtgas = (U*A*(Tpel-Tgas))/(Fgas*Yg);
+dtgas = (U*A*(Tpel-Tgas))/(Fgas*Yg); %equacao utilizada para determinar a variacao da temperatura do gas
 dtg=dtgas;
 end
